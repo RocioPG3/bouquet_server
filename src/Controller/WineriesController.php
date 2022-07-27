2e895ee6d9408c22ae0c24da86f685073e515ded
Rocio <rociopgarcia3@gmail.com>
2022-07-27 12:53:08 +0200

    pruebas de post y get en WineriesController

--- src/Controller/WineriesController.php
+++ src/Controller/WineriesController.php
@@ -7,10 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Repository\WineriesRepository;
-use App\Entity\Wineries;
 use App\Repository\UsersRepository;
+use App\Entity\Wineries;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\BrowserKit\Request;
+// use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class WineriesController extends AbstractController
 
@@ -30,7 +31,8 @@ class WineriesController extends AbstractController
                 'id' => $winerie->getId(),
                 'name' => $winerie->getName(),
                 'denomination' => $winerie->getDenomination(),
-                'location' => $winerie->getLocation()
+                'location' => $winerie->getLocation(),
+                'image' => $winerie->getImage(),
             ];
         }
 
@@ -39,9 +41,14 @@ class WineriesController extends AbstractController
         );
         
     
-}
+    }
 
-/**
+    //La siguiente ruta devuelve la bodega que le pasemos por parámetros (al hacer click en la bodega que queramos le paso id para que devuelva la info ampliada de dicha ruta en concreto en FichaPage)
+    
+    //http://localhost/bouquet_server/public/index.php/api/wineries/{id}
+
+
+    /**
      * @Route("/api/wineries/{id}", name="winerie", methods={"GET"}, requirements={"id": "\d+"} )
      */
     public function winerie(int $id, WineriesRepository $wineriesRepository): Response
@@ -53,7 +60,6 @@ class WineriesController extends AbstractController
             'denomination' => $winerie->getDenomination(),
             'address' => $winerie->getAddress(),
             'telephone' => $winerie->getTelephone(),
-            'email' => $winerie->getEmail(),
             'services' => $winerie->getServices(),
             'description' => $winerie->getDescription(),
             
@@ -65,7 +71,13 @@ class WineriesController extends AbstractController
         );
     }
 
-/**
+
+      /* Éste endpoint es para el formulario de reserva, para que devuelva el nombre de cada ruta para poderlo 
+    seleccionar en el desplegable: */
+
+    //http://localhost/bouquet_server/public/index.php/api/wineries/select
+
+    /**
      * @Route("/api/wineries/select", name="select", methods={"GET"})
      */
     public function select(WineriesRepository $wineriesRepository): Response
@@ -81,6 +93,51 @@ class WineriesController extends AbstractController
         );
     }
 
+
+    //Con este endpoint añadimos una entrada a la tabla Wineries: http://localhost/bouquet_server/public/index.php/api/winerie
+
+
+     /**
+     * @Route("/api/winerie", name="winerie", methods={"POST"})
+     */
+    public function add(Request $request, UsersRepository $usersRepository, EntityManagerInterface $em): Response
+    {
+        $data = json_decode($request->getContent(), true);       /*  true para que devuelva un array */
+
+        /* if($this->getUser()->getRide()){
+            return $this->json([
+                'message' => "Ride exists"
+            ],
+                Response::HTTP_FORBIDDEN
+            );
+        } */
+
+        $winerie= new Wineries();
+        
+        // $winerie->setUser($data['user']);
+        $winerie->setDenomination($data['denomination']);
+        $winerie->setName($data['name']);
+        $winerie->setLocation($data['location']);
+        $winerie->setAddress($data['address']);
+        $winerie->setTelephone($data['telephone']);
+        $winerie->setServices($data['services']);
+        $winerie->setDescription($data['description']);
+        $winerie->setImage($data['image']);
+
+        $winerie->setActive(true);
+        /* insertar la imagen aqui como otro elemento o fuera? */
+
+        /* $userId = $this->getUser()->getId();
+        $ride->setUser($userRepository->find($userId)); */
+
+
+        $em->persist($winerie);
+        $em->flush();
+
+        return $this->json($winerie, Response::HTTP_CREATED);
+
+    
+    }
 
 
 
